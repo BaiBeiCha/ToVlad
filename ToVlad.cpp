7924730d7a14e87514a54869f50b533a496267da
baibeicha <myvvot@gmail.com>
2024-11-13 14:25:20 +0300

    update

--- ToVlad.cpp
+++ ToVlad.cpp
@@ -8,22 +8,26 @@ int getMode()
 {
 	cout << "Enter which mode app need to work: \n1\n2\n";
 	int mode;
-	cin >> mode;
+	std::cin >> mode;
 	return mode;
 }
 
-string firstMode(int i)
+string firstMode(int i1, int i2)
 {
 	string path = "povelitel ershikov ";
-	path.append(to_string(i));
+	path.append(to_string(i1));
+	path.append("-");
+	path.append(to_string(i2));
 	path.append(".txt");
 	return path;
 }
 
-string secondMode(int i)
+string secondMode(int i1, int i2)
 {
 	string path = "C:\\Program Files\\povelitel ershikov ";
-	path.append(to_string(i));
+	path.append(to_string(i1));
+	path.append("-");
+	path.append(to_string(i2));
 	path.append(".txt");
 	return path;
 }
@@ -33,33 +37,45 @@ int main()
 {
 	int mode = getMode();
 
-	for (int i = 0; i < INT16_MAX; i++)
+	int amount;
+	cout << "Enter an amount of proccesses: ";
+	cin >> amount;
+	for (int i1 = 0; i1 < amount; i1++)
 	{
-		string fileName;
-
-		switch (mode)
+		cout << "Procces: " << i1 << endl;
+		for (int i2 = 0; i2 < INT16_MAX; i2++) 
 		{
-		case 1:
-			fileName = firstMode(i);
-			break;
-		case 2:
-			fileName = secondMode(i);
-			break;
-		default:
-			cout << "Nu ti i loh >>> mode 2";
-			break;
-		}
+			cout << "Modul: " << i2 << endl;
+			string fileName;
 
-		ofstream file;
-		file.open(fileName);
-		file << "合肥哦i委屈你和去海南富婆i去海南皮肤；哦i富农课外班否为白佛欧服为北方购入不过为给我换个配合各位韩国而我国和温哥华陪我刚好我俄国和而我国二位好个屁外观和物品IP规划为脾气IP规划IP把那个看来你公婆卡给你改变你货币规划破格就弄屁股琥珀金给老婆皇宫股票别弄不能够破格浓";
-		file.close();
+			switch (mode)
+			{
+			case 1:
+				fileName = firstMode(i1, i2);
+				break;
+			case 2:
+				fileName = secondMode(i1, i2);
+				break;
+			default:
+				cout << "Nu ti i loh >>> mode 2";
+				break;
+			}
 
-		if (i % 1000 == 0)
-		{
-			cout << "Was complite about " << 100.0 * i / INT16_MAX << "%!\n";
+			ofstream file;
+			file.open(fileName);
+			file << "合肥哦i委屈你和去海南富婆i去海南皮肤；哦i富农课外班否为白佛欧服为北方购入不过为给我换个配合各位韩国而我国和温哥华陪我刚好我俄国和而我国二位好个屁外观和物品IP规划为脾气IP规划IP把那个看来你公婆卡给你改变你货币规划破格就弄屁股琥珀金给老婆皇宫股票别弄不能够破格浓";
+			file.close();
+
+			if (i2 % 1000 == 0)
+			{
+				cout << "Was complite about " << 100.0 * i2 / INT16_MAX << "%!\n";
+			}
 		}
 	}
 
-	cout << "Was complite about 100%!\n\nSuccess!\nMade to Vlad" << endl;
+	cout << "Was complite 100%!\n\nSuccess!\nMade to Vlad" << endl;
+	char c;
+	std::cin >> c;
 }
+
+//TODO сделать больше файлов
